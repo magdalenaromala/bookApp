9961cda35a00278e98e408690822e4405fca71c2
magda <magda17095@wp.pl>
2019-05-31 09:32:28 +0200

    little change

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ let Footer = (props) => {
     return (
         <footer id="footer">
             <div className="container">
-                <p>Made with &hearts; by Hichem MED</p>
+                <p>Made with &hearts; by RomaLama</p>
             </div>
         </footer>
     )
